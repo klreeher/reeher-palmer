58cfde3f77ad59d665ed52abe7eb379175a1707e
Kate Reeher <kreeher@four51.com>
2018-06-04 08:57:37 -0500

    updated publish url, this will bork github pages i think

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://klreeher.github.io/reeher-palmer'
-RELATIVE_URLS = False
+SITEURL = 'https://reeher-palmer.net'
+RELATIVE_URLS = True
 
 PLUGIN_PATHS = ['plugins']
 THEME_PATHS = ['themes']
